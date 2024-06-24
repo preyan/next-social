0623b220feeec46c2864978dea726dd469890394
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2024-06-24 20:51:26 +0530

    chore: Update npm dependencies for analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
+import { Analytics } from "@vercel/analytics/react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +28,8 @@ export default function RootLayout({
           </div>
           <div className=" bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
             {children}
+            <Analytics />
+            <SpeedInsights />
           </div>
         </body>
       </html>
